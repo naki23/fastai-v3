83ce614262fe663f4e4782c73b76a05031b410dd
naki23 <nagy.kristof923@gmail.com>
2019-05-12 17:53:16 +0200

    cleanup

--- app/static/client.js
+++ app/static/client.js
@@ -25,7 +25,6 @@ function analyze() {
         if (this.readyState === 4) {
             var response = JSON.parse(e.target.responseText);
             el('result-label').innerHTML = `Result = ${response['result']}`;
-            el('result-label').innerHTML = `details = ${response['details']}`;
         }
         el('analyze-button').innerHTML = 'Analyze';
     }
