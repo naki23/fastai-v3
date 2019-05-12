b128d18101ba0ffbb27fdafd175428dcdf125b24
naki23 <nagy.kristof923@gmail.com>
2019-05-12 17:13:44 +0200

    results

--- app/server.py
+++ app/server.py
@@ -54,8 +54,8 @@ async def analyze(request):
     data = await request.form()
     img_bytes = await (data['file'].read())
     img = open_image(BytesIO(img_bytes))
-    prediction = learn.predict(img)
-    return JSONResponse({'result': str(prediction)})
+    prediction = learn.predict(img)[0]
+    return JSONResponse({'result': str(prediction)+str(learn.predict(img))})
 
 if __name__ == '__main__':
     if 'serve' in sys.argv: uvicorn.run(app=app, host='0.0.0.0', port=5042)
